2191bdaaf05f11b629c11b1f4e0d17581290b3fa
zongzhankui <zongzhankui@hotmail.com>
2017-06-08 00:29:43 +0800

    深入理解Java虚拟机

---
+++ Understanding-the-JVM/src/main/java/com/zkzong/jvm/ch02/HeapOOM.java
@@ -0,0 +1,20 @@
+package com.zkzong.jvm.ch02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * VM Args: -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ * Created by Zong on 2017/6/8.
+ */
+public class HeapOOM {
+    static class OOMObject {
+    }
+
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<OOMObject>();
+        while (true) {
+            list.add(new OOMObject());
+        }
+    }
+}
