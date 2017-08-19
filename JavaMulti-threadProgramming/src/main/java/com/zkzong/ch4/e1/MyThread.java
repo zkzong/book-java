1fe626a944fa3a9658022a70457e66c6a2eb785e
zongzhankui <zongzhankui@hotmail.com>
2017-08-19 21:32:56 +0800

    ch4

---
+++ JavaMulti-threadProgramming/src/main/java/com/zkzong/ch4/e1/MyThread.java
@@ -0,0 +1,15 @@
+package com.zkzong.ch4.e1;
+
+public class MyThread extends Thread {
+    private MyService service;
+
+    public MyThread(MyService service) {
+        super();
+        this.service = service;
+    }
+
+    @Override
+    public void run() {
+        service.testMethod();
+    }
+}
